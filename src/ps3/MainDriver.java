4966860756b9ecdf0f6cc379c3be6c1c94c17f8b
Narasimman <narasi.mit@gmail.com>
2015-11-09 16:29:20 -0500

    initial file read

---
+++ src/ps3/MainDriver.java
@@ -0,0 +1,55 @@
+package ps3;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainDriver {
+  private int number_of_reviewers;
+  private int util_s;
+  private int util_f;
+  private double p_s;
+  
+  private List<Reviewer> reviewers = new ArrayList<Reviewer>();
+  
+  MainDriver(String filePath) {
+    File inputFile = new File(filePath);
+    try(Scanner scanner = new Scanner(inputFile);) {
+      number_of_reviewers = scanner.nextInt();
+      util_s = scanner.nextInt();
+      util_f = scanner.nextInt();
+      p_s    = scanner.nextDouble();
+      
+      
+      for (int i = 0; i < number_of_reviewers; i++) {
+        Reviewer reviewer = new Reviewer(scanner.nextInt(), 
+            scanner.nextDouble(), scanner.nextDouble());
+        reviewers.add(reviewer);
+      }
+      
+    } catch(FileNotFoundException e) {
+      System.out.println("File not found exception");
+    }
+  
+  }
+  
+  
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Invalid argument! Please provide valid input path");
+      return;
+    }
+    
+    
+    
+    
+  }
+
+}
---
+++ src/ps3/Reviewer.java
@@ -0,0 +1,14 @@
+package ps3;
+
+public class Reviewer {
+  private int cost;
+  private double p_s;
+  private double p_f;
+  
+  Reviewer(int cost, double p_s, double p_f) {
+    this.cost = cost;
+    this.p_s  = p_s;
+    this.p_f  = p_f;
+  }  
+  
+}
