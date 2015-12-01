f6524d3071d5d9878a2b0e700a4bbd634f26102a
Narasimman Sairam <narasi.mit@gmail.com>
2015-12-01 18:42:22 -0500

    code to count word frequencyt

--- ps4/clustering.py
+++ ps4/clustering.py
@@ -1,43 +1,59 @@
 import re
 
 def parse_input():
-    input = open("corpus.txt")
-    stopwordsinput = open("stopwords.txt")
-    stopwords = stopwordsinput.read()
-    wordsList = []
-    
-    #read the words into a list
-    for str in input.readlines():
-        words = re.sub("[^\w]", " ",  str.lower()).split()
-        wordsList.extend(words)
+  input = open("corpus.txt")
+  stopwordsinput = open("stopwords.txt")
+  stopwords = stopwordsinput.read()
 
-    #discard all stop words and any word with length > 2
-    words = []
-    for word in wordsList:
-        if (word not in stopwords):
-            if(len(word) > 2):
-                words.append(word)
+  wordsList = []
+  texts = {}
+  blank = True
+  bio = ""
 
-    #get the count of each word
-    d = {}
-    for word in words:
+  #FIX ME: reads the input file and creates a map of author=>bio
+  for str in input.readlines():
+    str = str.strip()
+    if str:
+      str = str.lower()
+      if blank:
+        author = str
+        blank = False
+      else:
+        bio += str
+    else:
+      if bio != "" and author != "":
+        texts[author] = bio
+        author = ""
+        bio = ""
+        blank = True
+  texts[author] = bio
+
+  #read the words into a list
+  for str in texts.values():
+    words = re.sub("[^\w]", " ",  str.lower()).split()
+    wordsList.extend(words)
+
+  words = set()
+
+  #discard all stop words and any word with length > 2    
+  for word in wordsList:
+    if (word not in stopwords):
+      if(len(word) > 2):
+        words.add(word)
+
+  #get the count of each word
+  d = {}
+  for word in words:
+    for str in texts.values():
+      if (word in str):
         try:
-            d[word] += 1
+          d[word] += 1
         except:
-            d[word] = 1
+          d[word] = 1
 
-    # discard all words that appear more than half the number of times
-    count = len(words)
-    for k in d.keys():
-        if d[k] > count/2:
-            words.remove(d)
-    
-    for k in d.keys(): 
-        print "%s: %d" % (k, d[k])
-    
-    print len(words)
-    
-    #print words
-
+  for k in d.keys():
+    print "%s: %d" % (k, d[k])
+        
 if __name__ == "__main__":
-        parse_input()
+  parse_input()
+
