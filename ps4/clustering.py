9448aaee6e170142c728092ebb7af846ef999152
Narasimman Sairam <narasi.mit@gmail.com>
2015-12-02 00:49:16 -0500

    adding potter stemming code. yet to integrate

--- ps4/clustering.py
+++ ps4/clustering.py
@@ -1,6 +1,8 @@
 import re
+import math
+import sys
 
-def parse_input():
+def parse_input(N):
   input = open("corpus.txt")
   stopwordsinput = open("stopwords.txt")
   stopwords = stopwordsinput.read()
@@ -19,7 +21,7 @@ def parse_input():
         author = str
         blank = False
       else:
-        bio += str
+        bio += str + " "
     else:
       if bio != "" and author != "":
         texts[author] = bio
@@ -42,18 +44,47 @@ def parse_input():
         words.add(word)
 
   #get the count of each word
-  d = {}
+  words_reg = {}
+  words_to_text = {}
+  words_in_list = []
   for word in words:
-    for str in texts.values():
-      if (word in str):
+    index = 0
+    words_in_list = []
+    for biotext in texts.values():
+      index +=1
+         
+      if (word in biotext):
+        words_in_list.append(index)
         try:
-          d[word] += 1
+          words_reg[word] += 1
         except:
-          d[word] = 1
+          words_reg[word] = 1
+    #get the list in word in a map
+    words_to_text[word] = words_in_list
 
-  for k in d.keys():
-    print "%s: %d" % (k, d[k])
+  text_length = len(texts)
+
+  #discard all words that occur more than half the texts
+  for k in words_reg.keys():
+    if words_reg[k] > text_length/2:
+      words_reg.remove(k)
+  
+  #Calculate the weight
+  words_weight = {}
+  for word in words_reg:
+    words_weight[word] = -(math.log(float(words_reg[word])/float(text_length), 2))
+    
+
+  f = open('potter_input','w')
+  for k in words_weight.keys():
+    f.write(k + " ") # python will convert \n to os.linesep
+    #print "%s: %f" % (k, words_weight[k])
+  f.close() # you can omit in most cases as the destructor will call it
         
 if __name__ == "__main__":
-  parse_input()
+  if len(sys.argv) < 2:
+    sys.exit(-1)
+  N = sys.argv[1]
+  print N
+  parse_input(N)
 
