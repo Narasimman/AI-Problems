b493a137bbe3a6f093fa95364ca384686c994872
Narasimman Sairam <narasi.mit@gmail.com>
2015-12-08 03:08:53 -0500

    more changes

--- ps4/clustering.py
+++ ps4/clustering.py
@@ -5,19 +5,16 @@ from porterstemmer import PorterStemmer
 
 class Clustering:
   def __init__(self):
-    self.text = {}
+    self.texts = {}
     self.wordsList = {}
     self.porter = PorterStemmer()
     self.input = open("corpus.txt")
+    self.wordsWeight = {}
+    self.index_to_author = {}
 
-  def parse_input(self, N):
-    stopwordsinput = open("stopwords.txt")
-    stopwords = stopwordsinput.read()
-
-    self.texts = {}
+  def parse_input(self):
     blank = True
     bio = ""
-    index_to_author = {}
     index = 0
     #FIX ME: reads the input file and creates a map of author=>bio
     for line in self.input.readlines():
@@ -26,7 +23,7 @@ class Clustering:
         line = line.lower()
         if blank:
           author = line
-          index_to_author[index] = author
+          self.index_to_author[index] = author
           index +=1
           blank = False
         else:
@@ -44,6 +41,10 @@ class Clustering:
       words = re.sub("[^\w]", " ",  self.texts[key].lower()).split()
       self.wordsList[key] = words
 
+  def preprocess(self, N):
+    stopwordsinput = open("stopwords.txt")
+    stopwords = stopwordsinput.read()
+
     #discard all stop words and any word with length > 2    
     for key in self.wordsList:
       words = set()
@@ -53,6 +54,14 @@ class Clustering:
             words.add(word)
             self.wordsList[key] = words
 
+    text_length = len(self.texts)
+
+    for k in self.wordsList:
+      porter_words = set()
+      for word in self.wordsList[k]:
+        porter_words.add(self.porter.stem(word, 0, len(word)-1))
+      self.wordsList[k] = porter_words
+
     #get the count of each word
     words_reg = {}
     for key in self.wordsList:
@@ -61,55 +70,46 @@ class Clustering:
           words_reg[word] += 1
         except:
           words_reg[word] = 1
-
-    text_length = len(self.texts)
-
+    
     #discard all words that occur more than half the self.texts
     words_to_remove = []
     for k in words_reg.keys():
       if words_reg[k] > text_length/2:
         words_to_remove.append(k)
-
+        
     for key in self.wordsList:
       words = set()
       for word in self.wordsList[key]:
         if word not in words_to_remove:
           words.add(word)
-          self.wordsList[key] = words
+      self.wordsList[key] = words
 
     #Calculate the weight
-    words_weight = {}
+    self.wordsWeight = {}
     for word in words_reg:
-      words_weight[word] = -(math.log(float(words_reg[word])/float(text_length), 2))
+      self.wordsWeight[word] = -(math.log(float(words_reg[word])/float(text_length), 2))
 
 
     for i in range(text_length):
       for j in range(i):
-        set1 = self.wordsList[index_to_author[i]]
-        set2 = self.wordsList[index_to_author[j]]
+        set1 = self.wordsList[self.index_to_author[i]]
+        set2 = self.wordsList[self.index_to_author[j]]
 
         common = set1&set2
         if len(common) > 0:
           total_weight = 0
           for word in common:
-            total_weight += words_weight[word]
+            total_weight += self.wordsWeight[word]
             if total_weight > float(N):
-              print index_to_author[i],index_to_author[j],total_weight
+              print self.index_to_author[i], self.index_to_author[j],total_weight
             else:
               continue
-
-    f = open('potter_input','w')
-    for k in self.wordsList:
-      for word in self.wordsList[k]:
-        f.write(word + " ") # python will convert \n to os.linesep
-
-        #print k, wordsList[k]
-    f.close() # you can omit in most cases as the delineuctor will call it
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     sys.exit(-1)
   N = sys.argv[1]
   cluster = Clustering()
-  cluster.parse_input(N)
+  cluster.parse_input()
+  cluster.preprocess(N)
 
